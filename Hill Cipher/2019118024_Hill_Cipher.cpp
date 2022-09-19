df9dd6eb45ef2b6d7b58f078fe5d670f366d89c1
whibaek <jibaek0513@gmail.com>
2022-09-19 23:21:59 +0900

    hill cipher

--- Hill Cipher/2019118024_Hill_Cipher.cpp
+++ Hill Cipher/2019118024_Hill_Cipher.cpp
@@ -1,17 +1,15 @@
+#include <vector>
+#include <iostream>
+#include <algorithm>
+#include <string>
 #include <bits/stdc++.h>
+
 #define KEY_SZ 2 // 2 x 2 Matrix Key
 using namespace std;
 
-
-string encryption(string plaintext, string key) {
-	string result = plaintext;
-	vector<pair<int, bool>> idxArr; // 알파벳의 위치와, 대소문자 여부 (true -> 대문자, false -> 소문자)
+string extractAlpha(string plaintext, vector<pair<int, bool>> &idxArr) {
 	string alphatext = "";
-	int keyArr[2][2] = { key[0] - 65, key[2] - 65, key[1] - 65, key[3] - 65 };
-	int idx = 0;
-	vector<int> valArr;
-
-	// extract alphabet
+	
 	for (int i = 0; i < plaintext.size(); i++) {
 		char val = plaintext[i];
 		bool upper = false;
@@ -22,6 +20,20 @@ string encryption(string plaintext, string key) {
 			idxArr.push_back(make_pair(i, upper));
 		}
 	}
+
+	return alphatext;
+}
+
+string encryption(string plaintext, string key) {
+	string result = plaintext; 
+	vector<pair<int, bool>> idxArr; // 알파벳의 위치와, 대소문자 여부 (true -> 대문자, false -> 소문자)
+	string alphatext = ""; // 알파벳만 추출
+	int keyArr[2][2] = { key[0] - 65, key[2] - 65, key[1] - 65, key[3] - 65 };
+	int idx = 0;
+	vector<int> valArr;
+
+	// extract alphabet
+	alphatext = extractAlpha(plaintext, idxArr);
 
 	// making val vector
 	for (char val : alphatext) { valArr.push_back(val - 65); }
@@ -39,6 +51,7 @@ string encryption(string plaintext, string key) {
 		}
 	}
 
+
 	for (int i = 0; i < modArr.size(); i++) {
 		if (idxArr[i].second) { //대문자라면
 			result[idxArr[i].first] = (modArr[i]%26) + 65;
@@ -47,30 +60,25 @@ string encryption(string plaintext, string key) {
 			result[idxArr[i].first] = tolower((modArr[i]%26) + 65);
 		}
 	}
-	
 	return result;
 }
 
-void decryption(string plaintext, string key) {
-	string alphatext = ""; // 알파벳만 추출
+string decryption(string plaintext, string key) {
+	string result = plaintext; // result : 최종값
+	string alphatext = ""; // text에서 알파벳만 들어감
 	vector<pair<int, bool>> idxArr; // 알파벳의 위치와, 대소문자 여부 (true -> 대문자, false -> 소문자)
 	vector<int> valArr; // 암호화된 알파벳을 숫자로 변환한 값
+	int idx = 0;
 
 	// extract alphabet and Index 저장
-	for (int i = 0; i < plaintext.size(); i++) {
-		char val = plaintext[i];
-		bool upper = false;
-		if ((val >= 'a' && val <= 'z') || (val >= 'A' && val <= 'Z')) {
-			if (val >= 'A' && val <= 'Z') upper = true;
-
-			alphatext += toupper(val);
-			idxArr.push_back(make_pair(i, upper));
-		}
-	}
+	alphatext = extractAlpha(plaintext, idxArr);
 
 	// making val vector
 	for (char val : alphatext) { valArr.push_back(val - 65); }
+	vector<double> modArr(alphatext.size());
 
+
+	// 역행렬 만들기 start
 	double keyArr[2][2] = { (double)key[0] - 65, (double)key[2] - 65, 
 							(double)key[1] - 65, (double)key[3] - 65 }; // Key
 
@@ -82,24 +90,46 @@ void decryption(string plaintext, string key) {
 
 	double val = 1 / (a*d - b*c); // val -> 1/(ad-bc)
 	double reversArr[2][2] = { val * d, -(val * b), -(val * c), val * a }; //역행렬만들기
+	// 역행렬 만들기 끝
 
+	for (int i = 0; i < valArr.size(); i += KEY_SZ) {
+		int temp[2] = { valArr[i], valArr[i + 1] };
+		for (int j = 0; j < KEY_SZ; j++, idx++) {
+			for (int k = 0; k < KEY_SZ; k++) {
+				modArr[idx] += temp[k] * reversArr[k][j];
+			}
+		}
+	}
 	
-	double result[2] = { reversArr[0][0] * valArr[0] + reversArr[1][0] * valArr[1], reversArr[0][1] * valArr[0] + reversArr[1][1] * valArr[1] };
 
-	for (auto val : result) cout << val << " ";
-	cout << '\n';
+	for (int i = 0; i < modArr.size(); i++) {
+		if (idxArr[i].second) { //대문자라면
+			result[idxArr[i].first] = ((int)modArr[i] % 26) + 65;
+		}
+		else {
+			result[idxArr[i].first] = tolower(((int)modArr[i] % 26) + 65);
+		}
+	}
 
+	return result;
 }
 
 int main(void) {
 
 	string plaintext = "AB";
 	string key = "CEDB";
-	cout << encryption(plaintext, key) << '\n';
 
-	decryption("EB", key);
+	cout << "PlainText : " << plaintext << '\n';
+	cout << "Key : " << key << "\n\n";
 
-	//cout << encryption("SHORTEXAMPLE", "HILL") << '\n';
+	string entext = encryption(plaintext, key);
+	
+	cout << "Encryption : " << entext << "\n\n";
+
+	string detext = decryption(entext, key);
+
+	cout << "Decryption : " << detext << "\n";
+
 
 	return 0;
 }
